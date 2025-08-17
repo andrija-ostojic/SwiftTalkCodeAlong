5e80299fd4407cae4b0f314cb883c8bd98c2505b
Andrija Ostojić <andrija.ostojic@infinum.hr>
2025-08-17 09:04:34 +0200

    SwiftUI Layout - Lecture 5

--- SwiftUI Layout Explained/SwiftUILayout/ContentView.swift
+++ SwiftUI Layout Explained/SwiftUILayout/ContentView.swift
@@ -22,16 +22,23 @@ struct ContentView: View {
 
     var sample: some View_ {
         Ellipse_()
+            .frame(width: 150)
+            .frame(
+                minWidth: minWidth.enabled ? minWidth.0.rounded() : nil,
+                maxWidth: maxWidth.enabled ? maxWidth.0.rounded() : nil
+            )
             .overlay(GeometryReader_ { size in
                 Text_("\(Int(size.width))x\(Int(size.height))")
             })
             .border(NSColor.blue, width: 2)
-            .frame(width: width.rounded(), height: 300, alignment: .topLeading)
+            .frame(width: width.rounded(), height: 300)
             .border(NSColor.yellow, width: 2)
     }
 
     @State var opacity: Double = 0.5
     @State var width: CGFloat  = 300
+    @State var minWidth: (CGFloat, enabled: Bool) = (100, true)
+    @State var maxWidth: (CGFloat, enabled: Bool) = (300, true)
 
     var body: some View {
         VStack {
@@ -45,6 +52,16 @@ struct ContentView: View {
             HStack  {
                 Text("Width \(width.rounded())")
                 Slider(value: $width, in: 0...600)
+            }
+            HStack  {
+                Text("Min Width \(minWidth.0.rounded())")
+                Slider(value: $minWidth.0, in: 0...600)
+                Toggle("", isOn: $minWidth.enabled)
+            }
+            HStack  {
+                Text("Max Width \(maxWidth.0.rounded())")
+                Slider(value: $maxWidth.0, in: 0...600)
+                Toggle("", isOn: $maxWidth.enabled)
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
